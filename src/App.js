9de308f6b726aa85a311e3e092d51200f55125ac
tiansheng.zhu <tiansheng.zhu@amh-group.com>
2022-06-15 15:41:04 +0800

    useState && useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [id, setId] = useState(1)
+  const [age, setAge] = useState(1)
+
+  // React.StrictMode模式下挂载会执行两次
+  // useEffect(() => {
+  //   console.log('组件挂载完执行')
+  //   return () => {}
+  // }, [])
+  useEffect(() => {
+    console.log("组件挂载完及更新完执行")
+  })
+  // 相当于监听
+  // useEffect(() => {
+  //   console.log(id)
+  //   // setId(id => id + 1)
+  //   // const x = this.state.setId(x)
+  //   // return () => {
+  //   //   console.log('ddd')
+  //   // }
+  // }, [id, age])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setId(id => id + 1)}>addId</button>
+      { id }
+      <button onClick={() => setAge(age => age + 1)}>addAge</button>
+      { age }
     </div>
   );
 }
