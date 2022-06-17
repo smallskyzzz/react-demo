c6f1ba5d764e49d709d0b675c8ce846650243991
tiansheng.zhu <tiansheng.zhu@amh-group.com>
2022-06-17 14:32:54 +0800

    context

--- src/page/context/index.js
+++ src/page/context/index.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {createContext, useContext, useState} from "react";
 
 const context = () => {
+  const C = createContext(null)
+
+  // 爷组件
+  function GrandPa() {
+    const [n, setN] = useState(1)
+    return (
+      <C.Provider value={{n, setN}}>
+        grandPa   n: {n}
+        <Parent/>
+      </C.Provider>
+    )
+  }
+
   // 父组件
   function Parent() {
     return (
@@ -13,13 +26,22 @@ const context = () => {
 
   // 子组件
   function Child() {
+    const {n, setN} = useContext(C)
+
+    const add = () => {
+      setN(n => n+1)
+    }
+
     return (
-      <div>child</div>
+      <div onClick={add}>
+        <button>add</button>
+        child   爷组dsa件传递的n:{n}
+      </div>
     )
   }
 
   return (
-    <Parent />
+    <GrandPa />
   )
 }
 
